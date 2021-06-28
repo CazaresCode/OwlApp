11a5b5e2374b505230789e36d8a6f347a0558cbe
Alexandro Cazares <CazaresCode@gmail.com>
2021-06-28 11:50:20 -0400

    Add Student entity.

---
+++ Owl.Data/EntityModels/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Owl.Data.EntityModels
+{
+    public class Student : Person
+    {
+        [Required]
+        [Display(Name = "Program Enrolled")]
+        public ProgramType TypeOfProgram { get; set; }
+
+        [Required]
+        [Display(Name = "Has Paid Tuition")]
+        public bool HasPaidTuition { get; set; }
+
+        public virtual ICollection<Participation> Participations { get; set; }
+    }
+}
