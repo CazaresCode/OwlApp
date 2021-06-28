5e471fdc04dad8a25e0b4b9809ffc7603f580dad
Alexandro Cazares <CazaresCode@gmail.com>
2021-06-28 11:54:35 -0400

    Add Faculty Entity.

---
+++ Owl.Data/EntityModels/Faculty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Owl.Data.EntityModels
+{
+    public class Faculty
+    {
+        [Required]
+        [Display(Name = "Is Staff")]
+        public bool IsStaff { get; set; }
+
+        [Required]
+        [Display(Name = "Is Performing")]
+        public bool IsPerforming { get; set; }
+
+        public virtual ICollection<Participation> Participations { get; set; }
+    }
+}
