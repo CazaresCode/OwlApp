49a9204be44b41df26ccc05211b7b023c5bc10bb
Alexandro Cazares <CazaresCode@gmail.com>
2021-06-29 15:39:34 -0400

    Forgot to checkout... Create Meeting models: ListItem, Detail, Create.

---
+++ Owl.Models/MeetingModels/MeetingCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static EnumCommonLayer.MeetingTypeEnum;
+
+namespace Owl.Models.MeetingModels
+{
+    public class MeetingCreate
+    {
+        [Required]
+        [Display(Name = "Name of Meeting")]
+        public string NameOfMeeting { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        [Display(Name = "Start Time")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        [Display(Name = "End Time")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime EndTime { get; set; }
+
+        [Required]
+        [Display(Name = "Meeting Type")]
+        public MeetingType TypeOfMeeting { get; set; }
+    }
+}
---
+++ Owl.Models/MeetingModels/MeetingDetail.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static EnumCommonLayer.MeetingTypeEnum;
+
+namespace Owl.Models.MeetingModels
+{
+    public class MeetingDetail
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Name of Meeting")]
+        public string NameOfMeeting { get; set; }
+
+        public string Description { get; set; }
+
+        public string Location { get; set; }
+
+        [Display(Name = "Start Time")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime StartTime { get; set; }
+
+        [Display(Name = "End Time")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime EndTime { get; set; }
+
+        [Display(Name = "Meeting Type")]
+        public MeetingType TypeOfMeeting { get; set; }
+    }
+}
