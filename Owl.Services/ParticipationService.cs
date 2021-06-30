16c9e1865f08e878e20c4abdfa6fa0202a3441ef
Alexandro Cazares <CazaresCode@gmail.com>
2021-06-30 14:19:57 -0400

    Add GetParticipationById Method in Service.

--- Owl.Services/ParticipationService.cs
+++ Owl.Services/ParticipationService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Owl.Data;
+using Owl.Data.EntityModels;
+using Owl.Models.ParticipationModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,13 +18,61 @@ namespace Owl.Services
             _userId = userId;
         }
 
-        //public bool CreateParticipation (ParticipationCreate model)
-        //{
-        //    var entity =
-        //        new Participation()
-        //        {
+        public bool CreateParticipation(ParticipationCreate model)
+        {
+            var entity =
+                new Participation()
+                {
+                    OwnerId = _userId,
+                    PersonId = model.PersonId,
+                    MeetingId = model.MeetingId
+                };
 
-        //        }
-        //}
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Participations.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<ParticipationListItem> GetParticipations()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Participations
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                            e =>
+                                new ParticipationListItem
+                                {
+                                    Id = e.Id,
+                                    PersonId = e.PersonId,
+                                    MeetingId = e.MeetingId
+                                });
+
+                return query.ToArray();
+            }
+        }
+
+        public ParticipationDetail GetParticipationById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Participations
+                        .Single(e => e.Id == id && e.OwnerId == _userId);
+                return
+                    new ParticipationDetail
+                    {
+                        Id = entity.Id,
+                        PersonId = entity.PersonId,
+                        
+                        MeetingId = entity.MeetingId
+                    };
+            }
+        }
     }
 }
