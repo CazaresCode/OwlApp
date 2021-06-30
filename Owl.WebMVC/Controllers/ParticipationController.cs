65fbd05b5e7297c80c680a4f3eb9c6c60a082adf
Alexandro Cazares <CazaresCode@gmail.com>
2021-06-30 12:58:52 -0400

    Scaffold the files for Participation.

---
+++ Owl.WebMVC/Controllers/ParticipationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Owl.WebMVC.Controllers
+{
+    public class ParticipationController : Controller
+    {
+        // GET: Participation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
