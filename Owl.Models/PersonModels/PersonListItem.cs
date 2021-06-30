a2392822efd91f57684e812612b51fc7dc881ee3
Alexandro Cazares <CazaresCode@gmail.com>
2021-06-30 14:31:30 -0400

    Add Person Detail and ListItem.

--- Owl.Models/PersonModels/PersonListItem.cs
+++ Owl.Models/PersonModels/PersonListItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,12 @@ namespace Owl.Models.PersonModels
 {
     public class PersonListItem
     {
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
     }
 }
