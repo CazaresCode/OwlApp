297543962e78096e1a06545dc3d415646fddaf68
Alexandro Cazares <CazaresCode@gmail.com>
2021-06-30 13:18:25 -0400

    Add migration and OwnerId to data layer.

--- Owl.Services/ParticipationService.cs
+++ Owl.Services/ParticipationService.cs
@@ -8,5 +8,20 @@ namespace Owl.Services
 {
     public class ParticipationService
     {
+        private readonly Guid _userId;
+
+        public ParticipationService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        //public bool CreateParticipation (ParticipationCreate model)
+        //{
+        //    var entity =
+        //        new Participation()
+        //        {
+
+        //        }
+        //}
     }
 }
