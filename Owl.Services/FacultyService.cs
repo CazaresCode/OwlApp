1db2feb0ad7719b48dbacb6c5342f1327101b15c
Alexandro Cazares <CazaresCode@gmail.com>
2021-07-01 14:36:34 -0400

    Add facultyEdit and to service and Add first and last name to GetById.

--- Owl.Services/FacultyService.cs
+++ Owl.Services/FacultyService.cs
@@ -80,6 +80,8 @@ namespace Owl.Services
                     {
                         Id = entity.Id,
                         FullName = entity.FullName,
+                        FirstName = entity.FirstName,
+                        LastName = entity.LastName,
                         Email = entity.Email,
                         PhoneNumber = entity.PhoneNumber,
                         TypeOfInstrument = entity.TypeOfInstrument,
@@ -90,6 +92,32 @@ namespace Owl.Services
                         IsPerforming = entity.IsPerforming,
                         IsStaff = entity.IsStaff
                     };
+            }
+        }
+
+        public bool UpdateFaculty(FacultyEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Faculties
+                        .Single(e => e.Id == model.Id && e.OwnerId == _userId);
+
+                entity.FirstName = model.FirstName;
+                entity.LastName = model.LastName;
+                entity.Email = model.Email;
+                entity.PhoneNumber = model.PhoneNumber;
+                entity.TypeOfInstrument = model.TypeOfInstrument;
+                entity.StartTime = model.StartTime;
+                entity.EndTime = model.EndTime;
+                entity.HasFoodAllergy = model.HasFoodAllergy;
+                entity.FoodAllergy = model.FoodAllergy;
+                entity.HasFoodAllergy = model.HasFoodAllergy;
+                entity.IsPerforming = model.IsPerforming;
+                entity.IsStaff = model.IsStaff;
+
+                return ctx.SaveChanges() == 1;
             }
         }
 
