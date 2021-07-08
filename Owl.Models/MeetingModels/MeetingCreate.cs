f521ec41187e44317ad238a862c59c5c52245961
Alexandro Cazares <CazaresCode@gmail.com>
2021-07-08 10:49:32 -0400

    Add Date prop to models in Meeting for ListItem

--- Owl.Models/MeetingModels/MeetingCreate.cs
+++ Owl.Models/MeetingModels/MeetingCreate.cs
@@ -23,13 +23,13 @@ namespace Owl.Models.MeetingModels
         [Required]
         [Display(Name = "Start Time")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:MM}", ApplyFormatInEditMode = true)]
         public DateTime StartTime { get; set; }
 
         [Required]
         [Display(Name = "End Time")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:MM}", ApplyFormatInEditMode = true)]
         public DateTime EndTime { get; set; }
 
         [Required]
